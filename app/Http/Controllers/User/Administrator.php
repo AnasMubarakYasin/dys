eb2ee42e9a87c455a5e4d4bd25192f2db35f715a
WahyuSapurata <wm337708@gmail.com>
2023-03-02 22:52:41 +0800

    loginnapi blm berfungsi ple bang

--- app/Http/Controllers/User/Administrator.php
+++ app/Http/Controllers/User/Administrator.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Youth;
 
-class ClassName extends Controller
+class Administrator extends Controller
 {
-    public function dashboard() {
-        return view('pages.administrator.dashboard');
-    }
+  public function dashboard()
+  {
+    $youth = Youth::all()->count();
+    return view('pages.administrator.dashboard', ['yount' => $youth]);
+  }
 }
-
